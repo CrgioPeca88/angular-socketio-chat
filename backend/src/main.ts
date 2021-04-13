f1b8b5263016e7f78392d39f1d9d439a10ac6515
CrgioPeca88 <crgiopeca88@gmail.com>
2021-04-12 19:51:12 -0500

    [FEATURE] server with socket.io and cors rules started - main.ts adjusted

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,21 +1,29 @@
 // Dependencies
 import * as express from 'express';
 import { Request, Response } from 'express';
+import { createServer } from 'http';
 
 // Assets
-//import { PORT } from '@config/constants';
 import { PORT } from './config/constants';
 
 const app = express();
+const httpServer = createServer(app);
+const sio = require('socket.io')(httpServer, {
+  cors: {
+    origin: true,
+    credentials: true,
+    methods: ['GET', 'POST']
+  }
+});
 
 app.get('/', (req: Request, res: Response) => {
   res.send({
-    message: 'hello world! by CrgioPeca88',
+    message: 'hello world! by CrgioPeca888',
   });
 });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
   console.log(`====================================`);
   console.log('> Server started at http://localhost:' + PORT);
-  console.log(`====================================`)
+  console.log(`====================================`);
 });
