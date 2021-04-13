61c9432f0ca4d45951a0c2104dd0da614405ac0d
CrgioPeca88 <crgiopeca88@gmail.com>
2021-04-13 17:30:19 -0500

    [REAFCTOR] component folder created into app module - components folder created - app.component files moved to root folder into components folder

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { RootComponent } from './components/root/root.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    RootComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [RootComponent]
 })
 export class AppModule { }
